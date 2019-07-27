f7134e693d29db75f9e6fe47192c043ee9a8a24a
Akshay Gulhane <akshay.gulhane@outlook.com>
2019-07-27 22:23:19 +0530

    react build

---
+++ public/precache-manifest.034e8e7cbd07fb15c772f6fddba25f68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77482843b8dd92719719bac8886a4180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28834ea0b9eb858e3d13",
+    "url": "/static/css/main.55b4c05f.chunk.css"
+  },
+  {
+    "revision": "860d494be64d814c2519",
+    "url": "/static/js/2.4c3e3ec1.chunk.js"
+  },
+  {
+    "revision": "28834ea0b9eb858e3d13",
+    "url": "/static/js/main.4603745d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
